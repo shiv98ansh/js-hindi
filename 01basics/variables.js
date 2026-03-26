925f08bf47a5768d7655e0311d70625200b0c797
shiv98ansh <shivanshsanghi9@gmail.com>
2026-03-26 12:47:09 +0000

    container config done

--- 01basics/variables.js
+++ 01basics/variables.js
@@ -1,6 +1,9 @@
 const accountId=14553
 let accountEmail="shivansh@google.com"
 var accountPassword="12345"
+/*variable ka use mat karna*/
 accountCity="jaipur"
 accountId=2
+/*ese likh sakhte ho par kabhi mat likhna*/
 console.log(accountId);
+console.table([accountEmail,accountId,accountId,accountCity])
