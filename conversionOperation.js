40f8fddf86d7e6677a832ce4003a656f4625a2dd
shiv98ansh <shivanshsanghi9@gmail.com>
2026-03-29 05:01:04 +0000

    container config done

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,1 +1,21 @@
-let score=33
+let a=33
+let b ="33"
+let c="33abc"
+let d= null
+let e= undefined
+console.log(typeof a);
+let valueInNumber=Number(c)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//"33"=33
+//"33abc"=NaN
+//TRUE=1;FALSE=0;
+//undefined=NaN
+//NULL=0  
+let isLoggedIn=1
+let boleanIsLoggedIn=Bolean(isLoggedIn)
+console.log(boleanIsLoggedIn);
+//1=>true;0=>false
+//""=>false
+//"hitesh"=>true
+
