1115447010254cb3188703d4a442d9a1898f380a
shiv98ansh <shivanshsanghi9@gmail.com>
2026-03-31 13:41:23 +0000

    comparison

---
+++ comparison.js
@@ -0,0 +1,11 @@
+console.log("2">1);
+console.log(null>1);
+console.log(null==1);
+console.log(null>=1);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+
+// === it check data types
+
