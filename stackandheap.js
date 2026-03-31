5da3a7962a8c19a165650e34fe1a8f0eec8a20a5
shiv98ansh <shivanshsanghi9@gmail.com>
2026-03-31 16:22:55 +0000

    string

---
+++ stackandheap.js
@@ -0,0 +1,17 @@
+let myYoutubename= "shivanshdot com"
+let anothername ="myYoutubename"
+ anothername="chaiaurcode"
+ console.log(anothername);
+ console.log(myYoutubename);
+ 
+
+ //NON PRIMITIVE EX
+ let userOne ={
+    email: "shivgoogle.com",
+    upi:"user@ybl"
+ }
+ let userTwo=userOne
+ userTwo.email = "abcd.com"
+
+ console.log(userOne);
+ 
