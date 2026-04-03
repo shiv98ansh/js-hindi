6d5b668179c4a757732e98473076392a5ea75003
shiv98ansh <shivanshsanghi9@gmail.com>
2026-04-03 06:52:15 +0000

    string

---
+++ strings.js
@@ -0,0 +1,24 @@
+const name ="shivansh"
+const repocount = 50
+
+console.log(`hello my name is ${name} and my repocount is ${repocount}`);
+
+
+const gameName= new String('shivansh')
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+
+
+
+
+
+
